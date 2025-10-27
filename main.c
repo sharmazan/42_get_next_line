cf2931c46591f707442f6c6ae206e859b8017da3
Sergii Sharmazanov <ssharmaz@c2r2p8.42vienna.com>
2025-10-27 19:16:03 +0100

    Main function for testing

---
+++ main.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssharmaz <ssharmaz@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 15:02:20 by ssharmaz          #+#    #+#             */
+/*   Updated: 2025/10/27 18:08:09 by ssharmaz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+int	main(int ac, char **av)
+{
+	int		fd;
+	char	*str;
+
+	if (ac == 2)
+		fd = open(av[1], O_RDONLY);
+	else
+		fd = open("testfile", O_RDONLY);
+	printf("File descriptor: %d\n", fd);
+	for (int i = 0; i < 10; i++)
+	{
+		str = get_next_line(fd);
+		printf("%s", str);
+		free(str);
+	}
+	return (0);
+}
