4820ef2c1031e0b56ee50dd8f00277638fbda3be
Sergii Sharmazanov <ssharmaz@c2r2p8.42vienna.com>
2025-10-27 20:22:11 +0100

    Update test

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ssharmaz <ssharmaz@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 15:02:20 by ssharmaz          #+#    #+#             */
-/*   Updated: 2025/10/27 18:08:09 by ssharmaz         ###   ########.fr       */
+/*   Updated: 2025/10/27 20:14:45 by ssharmaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,11 @@ int	main(int ac, char **av)
 	if (ac == 2)
 		fd = open(av[1], O_RDONLY);
 	else
-		fd = open("testfile", O_RDONLY);
+		fd = 0;
 	printf("File descriptor: %d\n", fd);
 	for (int i = 0; i < 10; i++)
 	{
+		printf("\nline %d:", i);
 		str = get_next_line(fd);
 		printf("%s", str);
 		free(str);
